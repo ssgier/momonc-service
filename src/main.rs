1c1f66b4e9cff2fcdc6bf9ec5e48e70e1620aa00
Sandro Sgier <sandro.sgier@gmail.com>
2022-08-28 21:58:49 +0200

    Add tokio/tungstenite dependencies and skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,23 @@
 use env_logger::{self, Builder};
-use log::{debug, info, LevelFilter};
-use momonc_service::obj_func::ObjFunc;
-use serde::Serialize;
+use log::{info, LevelFilter};
+use serde_json::Value;
 use std::env;
 
-#[derive(Serialize)]
-struct TestInput {
-    x: f64,
-    y: f64,
-}
-
-fn main() {
+#[tokio::main]
+async fn main() {
     Builder::from_default_env()
         .filter(None, LevelFilter::Debug)
         .init();
+
     let run_cmd =
         env::var("MOMONC_OBJ_FUNC").expect("Environment variable MOMONC_OBJ_FUNC not set");
-    let obj_func = ObjFunc::new(&run_cmd);
-    info!("Running with objective function run command: {}", &run_cmd);
-    let test_val = TestInput { x: 0.5, y: 0.5 };
-    let obj_val = obj_func.call(&test_val);
-    debug!("Objective function value: {:?}", obj_val);
+
+    let spec_str = env::var("MOMONC_SPEC").expect("Environment variable MOMONC_SPEC not set");
+    let spec = serde_json::from_str::<Value>(&spec_str).expect("Failed to deserialize spec json");
+
+    info!(
+        "Application starting.\nObjective function command:\n\n{}\n\nSpec:\n\n{}",
+        run_cmd,
+        serde_json::to_string_pretty(&spec).unwrap()
+    );
 }
